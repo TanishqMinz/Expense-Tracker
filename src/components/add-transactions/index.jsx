5bdc92e98805bb04db8fec6455eb343e35841e7b
Tanishq <tanishqminz@gmail.com>
2024-06-23 15:36:41 +0530

    fixed expense turning into income when income is 0

--- src/components/add-transactions/index.jsx
+++ src/components/add-transactions/index.jsx
@@ -3,61 +3,67 @@ import { useContext, useState } from "react";
 import { GlobalContext } from "../../context";
 
 export default function TransactionForm({ onClose, isOpen }) {
+  const { formData, setFormData, handleFormSubmit } = useContext(GlobalContext);
+  const [error, setError] = useState('');
 
-    const { formData, setFormData, value, setValue, handleFormSubmit } = useContext(GlobalContext);
-    const [error, setError] = useState('');
-
-    function handleFormChange(event) {
-        const { name, value } = event.target;
-        if (name === 'amount' && value < 0) {
-            setError('Amount cannot be negative');
-        } else {
-            setError('');
-        }
-        setFormData({
-            ...formData,
-            [name]: value
-        });
+  function handleFormChange(event) {
+    const { name, value } = event.target;
+    if (name === 'amount' && value < 0) {
+      setError('Amount cannot be negative');
+    } else {
+      setError('');
     }
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        if (formData.amount < 0) {
-            setError('Amount cannot be negative');
-            return;
-        }
-        handleFormSubmit(formData);
-        onClose();
+  function handleTypeChange(value) {
+    setFormData({
+      ...formData,
+      type: value
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (formData.amount < 0) {
+      setError('Amount cannot be negative');
+      return;
     }
+    handleFormSubmit(formData);
+    onClose();
+  }
 
-    return (
-        <Modal isOpen={isOpen} onClose={onClose}>
-            <form onSubmit={handleSubmit}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Add new transaction</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <FormControl>
-                            <FormLabel>Enter description</FormLabel>
-                            <Input placeholder="Enter transaction description" name="description" type="text" onChange={handleFormChange} />
-                        </FormControl>
-                        <FormControl isInvalid={error}>
-                            <FormLabel>Enter amount</FormLabel>
-                            <Input placeholder="Enter transaction amount" name="amount" type="number" onChange={handleFormChange} />
-                            {error && <FormErrorMessage>{error}</FormErrorMessage>}
-                        </FormControl>
-                        <RadioGroup mt={'5'} value={value} onChange={setValue}>
-                            <Radio checked={formData.type === 'income'} value='income' colorScheme='blue' name='type' onChange={handleFormChange}>Income</Radio>
-                            <Radio ml={'2'} checked={formData.type === 'expense'} value='expense' colorScheme='red' name='type' onChange={handleFormChange}>Expense</Radio>
-                        </RadioGroup>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button mr={'4'} onClick={onClose}>Cancel</Button>
-                        <Button type="submit">Add</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </form>
-        </Modal>
-    )
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <form onSubmit={handleSubmit}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add new transaction</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl>
+              <FormLabel>Enter description</FormLabel>
+              <Input placeholder="Enter transaction description" name="description" type="text" onChange={handleFormChange} />
+            </FormControl>
+            <FormControl isInvalid={error}>
+              <FormLabel>Enter amount</FormLabel>
+              <Input placeholder="Enter transaction amount" name="amount" type="number" onChange={handleFormChange} />
+              {error && <FormErrorMessage>{error}</FormErrorMessage>}
+            </FormControl>
+            <RadioGroup mt="5" value={formData.type} onChange={handleTypeChange}>
+              <Radio value="income" colorScheme="blue">Income</Radio>
+              <Radio ml="2" value="expense" colorScheme="red">Expense</Radio>
+            </RadioGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button mr="4" onClick={onClose}>Cancel</Button>
+            <Button type="submit">Add</Button>
+          </ModalFooter>
+        </ModalContent>
+      </form>
+    </Modal>
+  );
 }
--- src/context/index.jsx
+++ src/context/index.jsx
@@ -2,23 +2,31 @@ import { createContext, useState } from "react";
 
 export const GlobalContext = createContext(null);
 
-export default function GlobalState({children}){
-     const [formData, setFormData] = useState({
-          type: 'income',
-          amount: 0,
-          description : ""
-     })
+export default function GlobalState({children}) {
+  const [formData, setFormData] = useState({
+    type: 'income',
+    amount: 0,
+    description: ""
+  });
 
-     const [value, setValue] = useState('expense')
-     const [totalExpense, setTotalExpense] = useState(0)
-     const [totalIncome, setTotalIncome] = useState(0)
-     const [allTransactions, setAllTranscations] = useState([])
+  const [totalExpense, setTotalExpense] = useState(0);
+  const [totalIncome, setTotalIncome] = useState(0);
+  const [allTransactions, setAllTransactions] = useState([]);
 
-     function handleFormSubmit(currentFormData){
-          if(!currentFormData.description || !currentFormData.amount) return
-          setAllTranscations([...allTransactions, {...currentFormData, id: Date.now()}])
+  function handleFormSubmit(currentFormData) {
+    if (!currentFormData.description || !currentFormData.amount) return;
+    setAllTransactions([...allTransactions, {...currentFormData, id: Date.now()}]);
+  }
 
-     }
-     console.log(allTransactions)
-     return <GlobalContext.Provider value={{formData, setFormData, totalExpense, setTotalExpense, totalIncome, setTotalIncome, value, setValue, allTransactions, setAllTranscations, handleFormSubmit}}>{children}</GlobalContext.Provider>
+  return (
+    <GlobalContext.Provider value={{
+      formData, setFormData,
+      totalExpense, setTotalExpense,
+      totalIncome, setTotalIncome,
+      allTransactions, setAllTransactions,
+      handleFormSubmit
+    }}>
+      {children}
+    </GlobalContext.Provider>
+  );
 }
